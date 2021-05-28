f0542233cf6763a829517a6911579f61cd644b1e
helloitsdaksh <helloitsdaksh007@gmail.com>
2021-05-28 11:56:54 +0530

    webscraping completed

--- webscracping.py
+++ webscracping.py
@@ -5,7 +5,7 @@ import pandas as pd
 words = []
 meaning = []
 agent = {"User-Agent":"Mozilla/5.0"}
-source  = requests.get("https://www.graduateshotline.com/gre-word-list.html",headers = agent).text
+source  = requests.get("https://www.graduateshotline.com/gre-word-list.html#x2",headers = agent).text
 soup = BeautifulSoup(source,'lxml')
 new_soup = soup.find('table',class_ = 'tablex border1')
 for article in new_soup.find_all('tr'):
@@ -16,5 +16,8 @@ for article in new_soup.find_all('tr'):
             count +=1
         elif count == 1:
             meaning.append(i.text)
-print(words)
+
+print(len(words))
+df = pd.DataFrame({'Word':words,'Meaing':meaning})
+df.to_excel("./Vocabulary-Builder/Word-Meaning.xlsx",index=False)
 
